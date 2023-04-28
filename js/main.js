3de8864e4c4055cae872beb4a72fdccada8ac2b0
yshu17 <magnificojack@gmail.com>
2023-04-28 12:28:12 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const myToast = new bootstrap.Toast(document.querySelector('.toast'));
 
 fetch('../json/csvjson.json')
   .then(response => response.json())
-  .then(data => words = data ) // массив объектов, созданных из файла JSON
+  .then(data => words = data) // массив объектов, созданных из файла JSON
   .catch(error => console.error(error));
 
 function searchWordsByFirstLetterInNameUa(letter) {
@@ -23,11 +23,11 @@ function searchWordsByFirstLetterInNameEn(letter) {
   }).map(word => ({ name: word.name_en, translate: word.name_ua, description: word.description_en }));
 }
 keycaps.forEach(keycap => {
-  keycap.addEventListener('click',() => {
+  keycap.addEventListener('click', () => {
 
     const keycapValue = keycap.querySelector('a').textContent;
 
-    if(/[а-яґєії]/i.test(keycapValue)) {
+    if (/[а-яґєії]/i.test(keycapValue)) {
       const wordsUa = searchWordsByFirstLetterInNameUa(keycapValue);
       if (wordsUa.length > 0) {
         clearWordList();
@@ -38,7 +38,7 @@ keycaps.forEach(keycap => {
       }
     }
 
-    if(/^[a-z\s]+$/i.test(keycapValue)) {
+    if (/^[a-z\s]+$/i.test(keycapValue)) {
       const wordsEn = searchWordsByFirstLetterInNameEn(keycapValue);
       if (wordsEn.length > 0) {
         clearWordList();
@@ -55,21 +55,21 @@ keycaps.forEach(keycap => {
 function addWordToDOM(word) {
   const randomID = Math.floor(Math.random() * 1000);
   const listItem = document.createElement('li');
-  listItem.classList.add('w-50','mb-2');
-  
+  listItem.classList.add('w-50', 'py-2');
+
   const dropdownLink = document.createElement('a');
-  dropdownLink.classList.add('mb-2');
-  dropdownLink.setAttribute('aria-controls',`${randomID}`);
-  dropdownLink.setAttribute('aria-expanded','false');
-  dropdownLink.setAttribute('role','button');
-  dropdownLink.setAttribute('href',`#${randomID}`);
-  dropdownLink.setAttribute('data-bs-toggle','collapse');
+  dropdownLink.classList.add('ms-2');
+  dropdownLink.setAttribute('aria-controls', `${randomID}`);
+  dropdownLink.setAttribute('aria-expanded', 'false');
+  dropdownLink.setAttribute('role', 'button');
+  dropdownLink.setAttribute('href', `#${randomID}`);
+  dropdownLink.setAttribute('data-bs-toggle', 'collapse');
   dropdownLink.innerText = word.name;
-  
+
   const dropdownBlock = document.createElement('div');
-  dropdownBlock.classList.add('collapse');
-  dropdownBlock.setAttribute('id',`${randomID}`);
- 
+  dropdownBlock.classList.add('collapse', 'ms-2');
+  dropdownBlock.setAttribute('id', `${randomID}`);
+
   dropdownBlock.innerHTML = `
     <h4 class="my-2">${word.translate}</h4>
     <p class="m-0 p-0">${word.description}</p>
